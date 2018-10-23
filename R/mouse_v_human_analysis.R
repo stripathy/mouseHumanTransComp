6438658f23cb75f5e4dca01532030bfb13b72af0
stripathy <stripat3@gmail.com>
2018-10-22 18:23:36 -0700

    adding comments to main r file

--- R/mouse_v_human_analysis.R
+++ R/mouse_v_human_analysis.R
@@ -51,7 +51,7 @@ mouse_micro_samples = mouseMeta %>% filter(cluster == 'Microglia Siglech')  %>% 
 
 micro_agg = compareMouseHumanExpr(mouse_micro_samples, human_micro_samples)
 
-# plot average expression profile of mouse genes vs human genes for microglia
+# plot average expression profile of mouse genes vs human genes for microglia - this is Fig 8b from the Hodge preprint
 ggplot(micro_agg, aes(x = mouseExpr, y = humanExpr)) + geom_point(alpha = .25) + 
   geom_abline(slope = 1) + 
   geom_abline(slope = 1, intercept = log2(10), color = 'blue') + 
@@ -64,22 +64,14 @@ match(mouse_human_homologous_genes$humanGene, names(human_micro_expr))
 human_l3a_samples = humanMeta %>% filter(str_detect('Exc L3-4 RORB CARM1P1', cluster)) %>% pull(sample_name)
 mouse_l3a_samples = mouseMeta %>% filter(str_detect('L5 IT VISp Hsd11b1 Endou', cluster))  %>% pull(sample_name)
 
-mouse_l3a_expr = mouseCPMLog[intersect(mouse_human_homologous_genes$mouseGene, rownames(mouseCPMLog)),mouse_l3a_samples] %>% rowMeans()
-human_l3a_expr = humanCPMLog[intersect(mouse_human_homologous_genes$humanGene, rownames(humanCPMLog)),human_l3a_samples] %>% rowMeans()
+l3a_agg_expr = compareMouseHumanExpr(mouse_l3a_samples, human_l3a_samples)
 
-mouse_l3a_expr = mouse_l3a_expr %>% as.data.frame() %>% tibble::rownames_to_column(var = 'mouseGene') 
-colnames(mouse_l3a_expr)[2] = 'mouseExpr'
-
-human_l3a_expr = human_l3a_expr %>% as.data.frame() %>% tibble::rownames_to_column(var = 'humanGene') 
-colnames(human_l3a_expr)[2] = 'humanExpr'
-
-l3a_agg = merge(mouse_human_homologous_genes, mouse_l3a_expr)
-l3a_agg = merge(l3a_agg, human_l3a_expr)
-
+# - this is Fig 8a from the Hodge preprint
 ggplot(l3a_agg, aes(x = mouseExpr, y = humanExpr)) + geom_point(alpha = .25) + 
   geom_abline(slope = 1) + 
   geom_abline(slope = 1, intercept = log2(10), color = 'blue') + 
   geom_abline(slope = 1, intercept = -log2(10), color = 'blue')
+
 
 human_l5_samples = humanMeta %>% filter(str_detect('Exc L4-5 FEZF2 SCN4B', cluster)) %>% pull(sample_name)
 mouse_l5_samples = mouseMeta %>% filter(str_detect(cluster, 'L5 PT VISp'))  %>% pull(sample_name)
